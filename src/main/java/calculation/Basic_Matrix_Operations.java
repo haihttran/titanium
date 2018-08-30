ee852e29912ab80a997c8ee6b34047ad99e69bbe
DESKTOP-1FHCNUV\tranh <tranhoangtruonghai@gmail.com>
2018-08-30 23:24:23 +0700

    Committed by Hai Tran.

--- src/main/java/calculation/Basic_Matrix_Operations.java
+++ src/main/java/calculation/Basic_Matrix_Operations.java
@@ -10,94 +10,193 @@ package calculation;
  * @author haitht
  */
 public class Basic_Matrix_Operations {
+
     public double[][] matrix;
-    
-    public Basic_Matrix_Operations(double[][] matrix){
+
+    public Basic_Matrix_Operations(double[][] matrix) {
         this.matrix = matrix;
     }
-    
-    public Basic_Matrix_Operations(int[][] matrix){
+
+    public Basic_Matrix_Operations(int[][] matrix) {
         this.matrix = convert(matrix);
     }
-    
-    public double[][] matrix_addition(double[][] m){        
+
+    public double[][] matrix_addition(double[][] m) {
         double[][] temp = new double[matrix.length][matrix[0].length];
-        if(matrix.length == m.length){
-            for(int i = 0; i < matrix.length; i++){
-                for(int j = 0; j < matrix[0].length; j++){
-                   temp[i][j] = matrix[i][j] + m[i][j];
+        if (matrix.length == m.length) {
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    temp[i][j] = matrix[i][j] + m[i][j];
                 }
             }
-        }
-        else {
+        } else {
             System.out.println("Unable to do matrix addition.");
         }
         return temp;
     }
-    
-    public double[][] matrix_addition(int[][] m){
+
+    public double[][] matrix_addition(int[][] m) {
         return this.matrix_addition(this.convert(m));
     }
-    
-    public double[][] matrix_subtraction(double[][] m){        
+
+    public double[][] matrix_subtraction(double[][] m) {
         double[][] temp = new double[matrix.length][matrix[0].length];
-        if(matrix.length == m.length){
-            for(int i = 0; i < matrix.length; i++){
-                for(int j = 0; j < matrix[0].length; j++){
-                   temp[i][j] = matrix[i][j] - m[i][j];
+        if (matrix.length == m.length) {
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    temp[i][j] = matrix[i][j] - m[i][j];
                 }
             }
-        }
-        else {
+        } else {
             System.out.println("Unable to do matrix addition.");
         }
         return temp;
     }
-    
-    public double[][] matrix_subtraction(int[][] m){
+
+    public double[][] matrix_subtraction(int[][] m) {
         return this.matrix_subtraction(this.convert(m));
     }
-    
-    public double[][] matrix_transpose(){
+
+    public double[][] matrix_transpose() {
         double[][] m = this.matrix;
         double[][] temp = new double[m.length][m[0].length];
-        for (int i=0; i<m.length; i++){
-            for(int j=0;j<m[0].length; j++)                
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
                 temp[j][i] = m[i][j];
+            }
         }
         return temp;
     }
-    
-    public double[][] matrix_multiplication(double[][] m){
+
+    public double[][] matrix_multiplication(double[][] m) {
         double[][] t = this.matrix;
         double[][] temp = new double[t.length][m[0].length];
-        if(t[0].length == m.length){
-            for(int i=0; i < t.length; i++){
-                for(int j=0; j < m[0].length; j++){
-                    for(int k=0; k < m.length; k++){
-                        temp[i][j] += t[i][k]*m[k][j];
+        if (t[0].length == m.length) {
+            for (int i = 0; i < t.length; i++) {
+                for (int j = 0; j < m[0].length; j++) {
+                    for (int k = 0; k < m.length; k++) {
+                        temp[i][j] += t[i][k] * m[k][j];
                     }
                 }
             }
         } else {
             System.out.println("Unable to do matrix multiplication.");
-        }            
+        }
         return temp;
     }
-    
-    public double[][] matrix_multiplication(int[][] m){
+
+    public double[][] matrix_multiplication(int[][] m) {
         return this.matrix_multiplication(this.convert(m));
     }
-    
-    private double[][] convert(int[][] matrix){
-        int m = matrix.length;
-        int n = matrix[0].length;
-        double[][] temp = new double[m][n];
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                temp[i][j] = matrix[i][j]*1.0;
+
+    public double[][] scalar_multiplication(double number) {
+        double[][] m = this.matrix;
+        double[][] temp = new double[m.length][m[0].length];
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                temp[i][j] = number * m[i][j];
             }
         }
         return temp;
+    }
+
+    public double[][] scalar_multiplication(int number) {
+        return this.scalar_multiplication(number * 1.0);
+    }
+
+    /*Calculate the mean*/
+    public double mean() {
+        // Calculate the sum
+        double[][] a = this.matrix;
+        double mean;
+        int n = this.matrix.length;
+        if (this.is_squared_matrix(a)) {
+            int sum = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    sum += a[i][j];
+                }
+            }
+            // Returning mean
+            return mean = sum / (n * n);
+        } else {
+            System.out.println("Not a squared matrix. Cannot find mean.");
+            return mean = 0.0;
+        }
+
+    }
+
+    // Calculate the variance
+    public double variance() {
+        double[][] a = this.matrix;
+
+        int n = this.matrix.length;
+        int sum = 0;
+        if (this.is_squared_matrix(a)) {
+            double m = this.mean();
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+
+                    // subtracting mean
+                    // from elements
+                    a[i][j] -= m;
+
+                    // a[i][j] = fabs(a[i][j]);
+                    // squaring each terms
+                    a[i][j] *= a[i][j];
+                }
+            }
+            // taking sum
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    sum += a[i][j];
+                }
+            }
+
+            return sum / (n * n);
+        } else {
+            System.out.println("Not a squared matrix. Cannot find variance");
+            return 0.0;
+        }
+    }
+
+    /*Calculate the standard deviation of the squared matrix*/
+    public double standard_deviation() {
+        return Math.sqrt(this.variance());
+    }
+
+    /*Matrix standardization*/
+    public void matrix_standardize() {
+        int rows = this.matrix.length;
+        int cols;
+        if (this.is_squared_matrix(this.matrix)) {
+            cols = rows;
+            double mu = this.mean();
+            double sig = this.standard_deviation();
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    this.matrix[i][j] = (this.matrix[i][j] - mu) / sig;
+                }
+            }
+        } else {
+            System.out.println("Not a squared matrix. "
+                    + "Cannot standardize the matrix.");
+        }
+    }
+
+    private double[][] convert(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        double[][] temp = new double[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                temp[i][j] = matrix[i][j] * 1.0;
+            }
+        }
+        return temp;
+    }
+
+    private boolean is_squared_matrix(double[][] m) {
+        return (m.length == m[0].length) ? true : false;
     }
 }
--- src/main/java/calculation/Basic_Stats_Operations.java
+++ src/main/java/calculation/Basic_Stats_Operations.java
@@ -15,13 +15,13 @@ public final class Basic_Stats_Operations {
 
     //sample set
     public double[] array;
-    
+
     //calculate mean of a set
     public Basic_Stats_Operations(double[] array) {
         this.array = array;
     }
-    
-    public Basic_Stats_Operations(int[] array){
+
+    public Basic_Stats_Operations(int[] array) {
         this.array = convert(array);
     }
 
@@ -62,52 +62,52 @@ public final class Basic_Stats_Operations {
         }
         return mode;
     }
-        
+
     //Calculate the standard deviation of the set
-    public double get_variance(){
+    public double get_variance() {
         double accumulation = 0;
         double mean = get_mean();
-        for (double d : array){
-            accumulation += Math.pow(d - mean, 2);            
+        for (double d : array) {
+            accumulation += Math.pow(d - mean, 2);
         }
-        return accumulation/(array.length-1);
+        return accumulation / (array.length - 1);
     }
-    
-    public double get_standard_deviation(){
+
+    public double get_standard_deviation() {
         return Math.sqrt(get_variance());
     }
-        
-    public double get_std_err(){
-        return get_standard_deviation()/Math.sqrt(array.length);
-    }    
-    
-    public double[] get_z_score(){
+
+    public double get_std_err() {
+        return get_standard_deviation() / Math.sqrt(array.length);
+    }
+
+    public double[] get_z_score() {
         double[] temp = new double[array.length];
         int counter = 0;
-        for (double d : array){
-            temp[counter] = (d - get_mean())/get_standard_deviation();
+        for (double d : array) {
+            temp[counter] = (d - get_mean()) / get_standard_deviation();
             counter++;
         }
         return temp;
     }
-    
-    public double get_z_score(double point){
-        return (point - get_mean())/get_standard_deviation();
+
+    public double get_z_score(double point) {
+        return (point - get_mean()) / get_standard_deviation();
     }
-    
-    public double get_z_score(int point){
-        return this.get_z_score(point*1.0);
+
+    public double get_z_score(int point) {
+        return this.get_z_score(point * 1.0);
     }
-    
+
     //Convert set of integers to equivalent floats
-    private double[] convert(int[] array){
+    private double[] convert(int[] array) {
         double[] temp = new double[array.length];
         int counter = 0;
-        for (int i : array){
-            temp[counter] = i*1.0;
+        for (int i : array) {
+            temp[counter] = i * 1.0;
             counter++;
         }
         return temp;
     }
-            
+
 }
